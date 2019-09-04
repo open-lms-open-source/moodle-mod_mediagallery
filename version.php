10749c76e120c0adadfc4b4e8c75bbd3094fc636
Adam Olley <adam.olley@blackboard.com>
2019-09-04 10:07:44 +0930

    v3.7.0.1 release

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019062700;
+$plugin->version   = 2019090400;
 $plugin->requires  = 2019051100;
 $plugin->cron      = 0;
 $plugin->component = 'mod_mediagallery';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.7.0.0';
+$plugin->release   = '3.7.0.1';
