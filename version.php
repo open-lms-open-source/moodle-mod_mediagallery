4a93e4613c080dcca56546b5dba56b8931863061
Adam Olley <adam.olley@openlms.net>
2023-06-02 00:31:57 +0930

    Bump version to 3.7.8

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022020701;
+$plugin->version   = 2022020702;
 $plugin->requires  = 2019051100;
 $plugin->cron      = 0;
 $plugin->component = 'mod_mediagallery';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.7.7';
+$plugin->release   = '3.7.8';
